1bf85e49aaaad3ffe62a9ce3b3f969412424463c
Inna Kim <inneekim@gmail.com>
2016-04-29 22:00:53 -0400

    correct one

--- Task4.3.py
+++ Task4.3.py
@@ -12,42 +12,56 @@ kp2, des2 = orb.detectAndCompute(img2,None)             #                    for
 
 bf = cv2.BFMatcher()
 matches = bf.knnMatch(des1,des2, k=2)                   # match interesting points
-print matches
 
-DIST_LIMIT = 150                                        # distance criterion
-inliers = []
-outliers = []
-for match1, match2 in matches:
-    if match1.distance < DIST_LIMIT:                    # decide inlier vs. outlier by distance  (4.3.b.I)
-        inliers.append(match1)
-    else:
-        outliers.append(match1)
+matSrc = []                                             # sort by distance
+for n, (match1, match2) in enumerate(matches):
+    matSrc.append([match1.queryIdx, match1.trainIdx, match1.distance])
+matSrc = sorted(matSrc, key=lambda tup: tup[2])    
 
+def isSeparated(m):                                     # is separated to points selected already 
+    for l in sel:
+        x1, y1 = kp1[matSrc[m][0]].pt
+        x2, y2 = kp1[matSrc[l][0]].pt
+        if (x1-x2)**2 + (y1-y2)**2 < 1600:  # 40**2     # eucledean distance of 40 pixels
+            return False
+    return True
+
+sel = []
 src = []
 dst = []
-for inlier in inliers:                                  # inlier points src(img1), dst(img2)
-    x1, y1 = kp1[inlier.queryIdx].pt
-    x2, y2 = kp2[inlier.trainIdx].pt
-    src.append([int(x1), int(y1)])
-    dst.append([int(x2), int(y2)])
+outliers = []
+for n in range(len(matSrc)):                            # select 8 points separated order by distance
+    if len(src) >= 8:                                   # for fundamental matrix F (fmat)
+        for m in range(n+1, len(matSrc)):               # not selected -> outliers
+            x1, y1 = kp1[matSrc[m][0]].pt
+            outliers.append([int(x1), int(y1)])
+        break
+    if isSeparated(n):
+        sel.append(n)
+        x1, y1 = kp1[matSrc[n][0]].pt
+        x2, y2 = kp2[matSrc[n][1]].pt
+        src.append([int(x1), int(y1)])
+        dst.append([int(x2), int(y2)])
 src = np.array(src)
 dst = np.array(dst)
 
-out = []
-for outlier in outliers:                                # outlier points of img1
-    x1, y1 = kp1[outlier.queryIdx].pt
-    out.append([int(x1), int(y1)])
-
 fmat = cv2.findFundamentalMat(src,dst,cv2.FM_LMEDS)[0]  # fundamental matrix by inlier points (src, dst)
 fmat = normalize(fmat, axis=0, norm='l2')               # normalized     (4.3.b.II)
+print fmat
 
 rgb1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2RGB)            # gray to rgb 
-for o in out:
-    cv2.circle(rgb1, tuple(o), 2, (0,255,0), -1)        # draw outliers with green dots on img1 (4.4.b.III)
+for outlier in outliers:
+    cv2.circle(rgb1, tuple(outlier), 2, (0,255,0), -1)  # draw outliers with green dots on img1 (4.4.b.III)
 plt.imshow(rgb1)
 plt.show()
-
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,               # Plot the corresponding epipolar lines (4.3.c)
-        inliers,None,matchColor=(255,0,0),singlePointColor=(0,255,0), flags=2)
-plt.imshow(img3)
+        
+N, M = img1.shape
+img3 = np.hstack((img1,img2))
+rgb3 = cv2.cvtColor(img3,cv2.COLOR_GRAY2RGB)            # gray to rgb 
+for n in range(7):                                      # 7 sets of matching points
+    cv2.circle(rgb3, tuple(src[n]), 5, (0,255,0))       
+    cv2.circle(rgb3, tuple((dst[n][0]+M,dst[n][1])), 5, (0,255,0))
+    cv2.line(rgb3, tuple(src[n]), tuple((dst[n][0]+M,dst[n][1])), (255,0,0), 1)
+    
+plt.imshow(rgb3)
 plt.show()
